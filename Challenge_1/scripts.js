e62b2092d6b25afd758be0512d603a6d2ac3fe5d
La-mar-s <sony.lamar.1995@gmail.com>
2023-07-09 21:04:57 +0200

    i do not know what else to do here

---
+++ Challenge_1/scripts.js
@@ -0,0 +1,35 @@
+
+// script.js
+
+const root_1 = document(order1),
+const biscuits_1 = document(biscuits),
+const donuts_1 = document(donuts),
+const pancakes_1 = document(pancakes),
+const status_1 = document(status),
+
+const root_2 = document(order2),
+const biscuits_2 = document(biscuits),
+const donuts_2 = document(donuts),
+const pancakes_2 = document(pancakes),
+const status_2 = document(status),
+
+const root_3 = document(order3),
+const biscuits_3 = document(biscuits),
+const donuts_3 = document(donuts),
+const pancakes_3 = document(pancakes),
+const status_3 = document(status),
+
+1-biscuits = root_1.biscuits,
+1-donuts = root_1.donuts,
+1-pancakes = root_1.pancakes,
+1-status = root_1.status ? Delivered = Pending,
+
+2-biscuits= root_2.biscuits,
+2-donuts = root_2.donuts,
+2-pancakes = root_2.pancakes,
+2-status = root_2-root.status ? Delivered = Pending
+
+3-biscuits= 3-root.biscuits,
+3-donuts = 3-root.donuts,
+3-pancakes = 3-root.pancakes,
+3-status = 3-root.status ? Delivered = Pending
